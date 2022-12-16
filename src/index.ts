4c7fccabee49d4e2a0f69ec3fa0a1fceeb4a4a2a
yamiteru <yamiteru@icloud.com>
2022-12-16 06:07:47 +0100

    Change Uint8Array back to string

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import crypto from "crypto";
+
 const SALT_SIZE = 32;
 
 const concat = (
@@ -26,31 +28,59 @@ const equal = (a: Uint8Array, b: Uint8Array) => {
 	return true;
 }
 
+const stringToUint8Array = (value: string) => {
+	const length = value.length;
+	const array =	new Uint8Array(length);
+
+	for(let i = 0; i < length; ++i) {
+		array.set([value.charCodeAt(i)], i);
+	}
+
+	return array;
+};
+
+const Uint8ArrayToString = (value: Uint8Array) => {
+	const length = value.byteLength;
+	let string = "";
+
+	for(let i = 0; i < length; ++i) {
+		string += String.fromCharCode(value[i]);	
+	}
+
+	return string;
+};
+
 export const hash = async (
 	password: string,
 	salt = crypto.getRandomValues(new Uint8Array(SALT_SIZE))
-) => 
-	new Uint8Array(
+) => {
+	return Uint8ArrayToString(
 		concat(
 			salt,
 			new Uint8Array(
 				await crypto.subtle.digest(
 					"SHA-512",
-					concat(salt, new TextEncoder().encode(password))
+					concat(salt, stringToUint8Array(password))
 				)
 			)
 		)
 	);
+};
 
 export const verify = async (
-	hashedPassword: Uint8Array,
+	hashedPassword: string,
 	unhashedPassword: string
 ) => {
+	const password = stringToUint8Array(hashedPassword);
+
 	return equal(
-		await hash(
-			unhashedPassword,
-			new Uint8Array(hashedPassword).subarray(0, SALT_SIZE)
+		stringToUint8Array(
+			await hash(
+				unhashedPassword,
+				password.subarray(0, SALT_SIZE)
+			)
 		),
-		hashedPassword	
+		password
 	);
 }
+
