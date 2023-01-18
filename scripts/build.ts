c2bef3e21a99f27a1114cc338b8c0d26aa7226de
VietnamecDevelopment <vietnamec@email.vietdev.tk>
2023-01-18 22:03:54 +0100

    Added Code Formating, Formated it <33

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,44 +1,38 @@
 import { build, BuildOptions } from "esbuild";
 import { Generator } from "npm-dts";
-import { 
-	source,	
-	main as cjs, 
-	module as esm, 
-	types 
-} from "../package.json";
+import { source, main as cjs, module as esm, types } from "../package.json";
 
 const shared: BuildOptions = {
-	bundle: true,
-	entryPoints: [source],
-	logLevel: "info",
-	minify: true,
-	sourcemap: true,
-	platform: "node"
+  bundle: true,
+  entryPoints: [source],
+  logLevel: "info",
+  minify: true,
+  sourcemap: true,
+  platform: "node",
 };
 
-const buildEsm = () => build({
-	...shared,
-	format: "esm",
-	outfile: esm,
-	target: ["esnext", "chrome97"],
-});
+const buildEsm = () =>
+  build({
+    ...shared,
+    format: "esm",
+    outfile: esm,
+    target: ["esnext", "chrome97"],
+  });
 
-const buildCjs = () => build({
-	...shared,
-	format: "cjs",
-	outfile: cjs,
-	target: ["esnext", "chrome97"],
-});
+const buildCjs = () =>
+  build({
+    ...shared,
+    format: "cjs",
+    outfile: cjs,
+    target: ["esnext", "chrome97"],
+  });
 
-const buildTypes = () => new Generator({
-	entry: source,
-	output: types,
-}).generate();
+const buildTypes = () =>
+  new Generator({
+    entry: source,
+    output: types,
+  }).generate();
 
 (async () => {
-	await Promise.all([
-		buildEsm(),
-		buildCjs(),
-		buildTypes()
-	]);
+  await Promise.all([buildEsm(), buildCjs(), buildTypes()]);
 })();
